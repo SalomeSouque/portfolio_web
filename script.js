a9331963aaed11a32efafab92017689cecfc01b8
SalomeSouque <salome.souque@gmail.com>
2025-01-31 20:16:41 +0100

    v6

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ fetch("./texte.json")   //J'appel mon fichier json qui est sur une autre page
     })
     .then(data =>{
 
-    console.log(data);
+    // console.log(data);
     
 
     
@@ -17,46 +17,83 @@ fetch("./texte.json")   //J'appel mon fichier json qui est sur une autre page
     for (let key in data) {
 
         let keys = Object.keys(data);
-        console.log(keys);
+        // console.log(keys);
 
-        let div = document.getElementById ("Div");
+        let divjs = document.getElementById ("DivJS");
         let indeximage =0;
+        let indexlink =0;
+        // let cardall = document.createElement ("div");
+        let tableau = [];
+        // myArray.push(myObject)
 
         data.web.forEach(element => {
-
+            indexlink ++
             let card = document.createElement ("div");
-            card.className = "card";
-            div.appendChild(card);
-        
+            card.id = "cardjs"+indexlink
+            card.className = "cardjs";
+            divjs.appendChild(card);
+            tableau.push(card)
+            
+
+            // card[1].addEventListener("click", function (eve) {        //Je crée une fonction qui va servir à supprimer le li lors du click sur le bouton
+            //     eve.preventDefault ();                              //           
+            //     window.location.href = 'https://www.1formatik.com/creer-lien-hypertexte-html-js';                                 // Je met mon li sur mon ul
+            // })
+            
+            // Array.prototype.push.apply(card)
+            // console.log(tableau);
+            
+            let test = document.createElement ("a");
+            test.className = "testlien";
+            test.src = element["a-url"];
+            card.appendChild(test)
+// console.log(card);
+
+
         indeximage++
         let image = document.createElement ("div");
         image.id = "image"+indeximage;
         image.textContent = element.image;
         card.appendChild(image);
-        console.log(image);
+        // console.log(image);
         
         
         let trait = document.createElement("div");
-        trait.className = "trait";
+        trait.className = "traitjs";
         trait.textContent = element.trait;
         card.appendChild(trait);
 
         let titre = document.createElement("h2");
-        titre.className = "titre";
+        titre.className = "titrejs";
         titre.textContent = element.titre;
         card.appendChild(titre);
 
         let description = document.createElement("p");
-        description.className = "description";
+        description.className = "descriptionjs";
         description.textContent = element.description;
         card.appendChild(description);
 
      
-        
+    
 
         });
-        
-        
+        // tableau.push(divjs)
+            console.log(tableau);
+                tableau[1].addEventListener("click", function (eve) {        //Je crée une fonction qui va servir à supprimer le li lors du click sur le bouton
+                eve.preventDefault ();
+                                              //           
+                window.location.href = 'https://yuka.io/';                                 // Je met mon li sur mon ul
+            })
+
+            function opentab (params) {
+                
+            }
+
+
+        // let test = document.createElement (a);
+        // text.className = "testlien";
+        // test.src = element["a-url"];
+        // card.appendChild(test)
     }})
     
   
